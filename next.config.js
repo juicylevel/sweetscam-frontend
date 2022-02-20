4c870f37b2a91b03083613046c26afe82ad71597
Yuri Rykov <rykov87yuri@gmail.com>
2022-02-20 14:57:18 +0300

    feat: set up assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    assetPrefix: isProd ? '/sweetscam-frontend/' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
